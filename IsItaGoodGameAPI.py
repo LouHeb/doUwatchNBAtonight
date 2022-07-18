bcbb7cd86c9abe1ad8af8234ad0b71e7e864fb15
LouHeb <78810964+LouHeb@users.noreply.github.com>
2022-07-18 21:46:56 +0200

    Update IsItaGoodGameAPI.py

--- IsItaGoodGameAPI.py
+++ IsItaGoodGameAPI.py
@@ -17,7 +17,7 @@ custom_headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
     'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language' = 'en-US,en;q=0.8,af;q=0.6'
+    'Accept-Language' : 'en-US,en;q=0.8,af;q=0.6'
 }
 
 
