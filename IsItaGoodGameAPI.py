e223d4b68fda21c711d64a2970634575d39cb5f3
LouHeb <louis.hebrard@insa-lyon.fr>
2022-07-23 15:17:46 +0200

    07/22/2022

--- IsItaGoodGameAPI.py
+++ IsItaGoodGameAPI.py
@@ -317,18 +317,18 @@ if LastDay!=Today:
 
 from git import Repo
 
-PATH_OF_GIT_REPO = r''  # make sure .git folder is properly configured
+PATH_OF_GIT_REPO = r'~/github/doUwatchNBAtonight/'  # make sure .git folder is properly configured
 COMMIT_MESSAGE = Today
 
 def git_push():
-    try:
-        repo = Repo(PATH_OF_GIT_REPO)
-        repo.git.add(update=True)
-        repo.index.commit(COMMIT_MESSAGE)
-        origin = repo.remote(name='gh-pages')
-        origin.push()
-    except:
-        print('Some error occured while pushing the code')    
+
+    repo = Repo(PATH_OF_GIT_REPO)
+    repo.git.add(update=True)
+    repo.index.commit(COMMIT_MESSAGE)
+    origin = repo.remote(name='gh-pages')
+    origin.push()
+#    except:
+#        print('Some error occured while pushing the code')    
 
 git_push()
 
