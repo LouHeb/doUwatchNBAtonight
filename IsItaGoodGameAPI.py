59f56fd4a61042153f471a3cadc460e0f3f00da9
LouHeb <louis.hebrard@insa-lyon.fr>
2022-07-23 15:18:25 +0200

    07/22/2022

--- IsItaGoodGameAPI.py
+++ IsItaGoodGameAPI.py
@@ -325,7 +325,7 @@ def git_push():
     repo = Repo(PATH_OF_GIT_REPO)
     repo.git.add(update=True)
     repo.index.commit(COMMIT_MESSAGE)
-    origin = repo.remote(name='gh-pages')
+    origin = repo.remote(name='origin')
     origin.push()
 #    except:
 #        print('Some error occured while pushing the code')    
