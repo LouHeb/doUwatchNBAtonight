ac2acfa3cdbb5603164d27c5bfbdf20dbe0b0e37
LouHeb <louis.hebrard@insa-lyon.fr>
2022-07-23 15:31:08 +0200

    07/22/2022

--- IsItaGoodGameAPI.py
+++ IsItaGoodGameAPI.py
@@ -178,6 +178,7 @@ item+=2
 Y = Line[item:item+4]
 
 LastDay = M+'/'+D+'/'+Y
+LastDayp1 = datetime.strftime(datetime.strptime(LastDay, "%m/%d/%Y") + timedelta(1),"%m/%d/%Y")
 
 if LastDay!=Today:
     file = open("index.md","w") 
@@ -186,7 +187,7 @@ if LastDay!=Today:
     for league in Leagues:    
     
         # --- Extract the games of yesterday
-        StudiedGames = leaguegamefinder.LeagueGameFinder(player_or_team_abbreviation='T',date_from_nullable = LastDay,date_to_nullable = Today, league_id_nullable = ligId[league], outcome_nullable = "W")
+        StudiedGames = leaguegamefinder.LeagueGameFinder(player_or_team_abbreviation='T',date_from_nullable = LastDayp1,date_to_nullable = Today, league_id_nullable = ligId[league], outcome_nullable = "W")
         #StudiedGames = leaguegamefinder.LeagueGameFinder(player_or_team_abbreviation='T',season_nullable = '2021-22', league_id_nullable = ligId[league], outcome_nullable = "W")
         
         df = StudiedGames.get_data_frames()
