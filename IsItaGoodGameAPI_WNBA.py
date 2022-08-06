62dbc46ba0dd5a993e0b59b0c46931dcedbcf3c5
LouHeb <78810964+LouHeb@users.noreply.github.com>
2022-08-06 14:09:45 +0200

    From last run to today

--- IsItaGoodGameAPI_WNBA.py
+++ IsItaGoodGameAPI_WNBA.py
@@ -205,7 +205,7 @@ if 1==1:#LastDay!=Today:
     for league in Leagues:    
     
         # --- Extract the games of yesterday
-        StudiedGames = leaguegamefinder.LeagueGameFinder(player_or_team_abbreviation='T',date_from_nullable = Today,date_to_nullable = Today, league_id_nullable = ligId[league], outcome_nullable = "W")
+        StudiedGames = leaguegamefinder.LeagueGameFinder(player_or_team_abbreviation='T',date_from_nullable = LastDayp1,date_to_nullable = Today, league_id_nullable = ligId[league], outcome_nullable = "W")
         #StudiedGames = leaguegamefinder.LeagueGameFinder(player_or_team_abbreviation='T',season_nullable = '2021-22', league_id_nullable = ligId[league], outcome_nullable = "W")
         
         df = StudiedGames.get_data_frames()
