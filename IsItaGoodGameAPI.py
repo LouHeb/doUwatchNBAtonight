47b9a8865e13863f4dc13337ab1d1855087d8d25
LouHeb <78810964+LouHeb@users.noreply.github.com>
2024-04-27 16:31:07 +0200

    test12

--- IsItaGoodGameAPI.py
+++ IsItaGoodGameAPI.py
@@ -518,6 +518,7 @@ for ld in LesDates:
                     if Ledf is None:
                         Ledf = pd.DataFrame(columns = list(d.keys()))
                     Ledf = Ledf.append(d, ignore_index=True)
+                print(Ledf)
                 except:
                     continue
             
