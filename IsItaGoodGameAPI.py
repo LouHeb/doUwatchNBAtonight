7e163133f2ae0763d4aed38c88e17b989468416e
LouHeb <78810964+LouHeb@users.noreply.github.com>
2024-04-27 18:34:58 +0200

    te

--- IsItaGoodGameAPI.py
+++ IsItaGoodGameAPI.py
@@ -514,7 +514,7 @@ for ld in LesDates:
                 d[f'{t2}_ACTION'] = " "#row[list(DDD.columns)[5]]
                 if DF is None:
                     DF = pd.DataFrame(columns = list(d.keys()))
-                DF = DF.concat([DF, d], ignore_index=True)
+                DF = concat([DF, d], ignore_index=True)
 
                 print('----------')
                 print(DF)
